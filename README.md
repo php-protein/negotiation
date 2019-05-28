6fab9abad9f5f5cb3e20e50260585902c7d0a46f
Stefano Azzolini <stefano.azzolini@jobtome.com>
2019-05-28 15:25:56 +0200

    Initial Upload 🚀

---
+++ README.md
@@ -0,0 +1,67 @@
+<p align=center><img height=150 src="https://raw.githubusercontent.com/php-protein/docs/master/assets/protein-large.png"></p>
+
+# Protein | Negotiation
+## A module for handling Content Negotiation.
+
+> See Reference : [RFC 7231](https://tools.ietf.org/html/rfc7231)
+
+### Install
+---
+
+```
+composer require protein/negotiation
+```
+
+Require the class via :
+
+```php
+use Protein\Negotiation;
+```
+
+
+### Get best match between need and offerings
+---
+
+> **Note:** You can use `*` as wildcards for matching a family of choices.
+
+
+```php
+$need  = 'image/*;q=0.9,*/*;q=0.2';
+$offer = 'text/html,svg/xml,image/svg+xml';
+echo Negotiation::bestMatch($need,$offer);
+```
+
+```
+image/svg+xml
+```
+
+### Preferred and best match
+---
+
+Negotiation class automatically orders by priority based on `q` parameter.
+ 
+```php
+$negotiatior = new Negotiation('en-US;q=0.3,it,en;q=0.4,es;q=0.9,de');
+```
+
+You can obtain the preferred response via the `preferred` method.
+
+```php
+echo $negotiatior->preferred();
+```
+
+```
+it
+```
+
+Or get the best match against another RFC7231 query
+
+```php
+echo $negotiatior->best('es,en-US');
+```
+
+```
+es
+```
+
+`false` will be returned if no match can be found.
---
+++ src/Negotiation.php
@@ -0,0 +1,70 @@
+<?php declare(strict_types=1);
+
+/**
+ * Negotiation
+ *
+ * A module for handling Content Negotiation.
+ *
+ * @package Protein
+ * @author  "Stefano Azzolini"  <lastguest@gmail.com>
+ */
+
+namespace Protein;
+
+class Negotiation {
+    protected $list;
+
+    public static function parse($query) {
+        $list = new \SplPriorityQueue();
+        array_map(function ($e) use ($list) {
+            preg_match_all('(([^;]+)(?=\s*;\s*(\w+)\s*=\s*([^;]+))*)', $e, $p);
+            $params = array_map(
+          'trim',
+          array_merge(
+          [ 'type' => current($p[0]) ],
+          array_combine($p[2], $p[3])
+      )
+      );
+            unset($params['']);
+            $params['q'] = isset($params['q']) ? 1.0*$params['q'] : $params['q'] = 1.0;
+            $list->insert($params, $params['q']);
+        }, preg_split('(\s*,\s*)', $query));
+        return array_values(iterator_to_array($list));
+    }
+
+    public static function bestMatch($acceptables, $choices) {
+        return (new self($acceptables))->best($choices);
+    }
+
+    public function __construct($query) {
+        $this->list = self::parse(trim($query));
+    }
+
+    public function preferred() {
+        return self::encodeParsedValue(current($this->list));
+    }
+
+    protected static function encodeParsedValue($parsed) {
+        unset($parsed['q']);     // Hide quality key from output
+        $type = $parsed['type']; // Extract type
+        unset($parsed['type']);
+        return implode(';', array_merge([$type], array_map(function ($k, $v): string {
+            return "$k=$v";
+        }, array_keys($parsed), $parsed)));
+    }
+
+    public function best($choices) {
+        $_choices  = self::parse(trim($choices));
+        foreach ($this->list as $accept) {
+            foreach ($_choices as $choice) {
+                if (preg_match('('.strtr(
+            $accept["type"],
+            [ '.' => '\.', '+' => '\+', '*' => '.+' ]
+        ).')', $choice["type"])) {
+                    return self::encodeParsedValue($choice);
+                }
+            }
+        }
+        return false;
+    }
+}
