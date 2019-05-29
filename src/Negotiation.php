72a11afdc9779263eb182f8d8242dfe80cc76584
Stefano Azzolini <stefano.azzolini@jobtome.com>
2019-05-29 16:36:57 +0200

    Release 1.0.1

--- src/Negotiation.php
+++ src/Negotiation.php
@@ -16,20 +16,20 @@ class Negotiation {
 
     public static function parse($query) {
         $list = new \SplPriorityQueue();
-        array_map(function ($e) use ($list) {
-            preg_match_all('(([^;]+)(?=\s*;\s*(\w+)\s*=\s*([^;]+))*)', $e, $p);
-            $params = array_map(
+        \array_map(function ($e) use ($list) {
+            \preg_match_all('(([^;]+)(?=\s*;\s*(\w+)\s*=\s*([^;]+))*)', $e, $p);
+            $params = \array_map(
           'trim',
-          array_merge(
+          \array_merge(
           [ 'type' => current($p[0]) ],
-          array_combine($p[2], $p[3])
+          \array_combine($p[2], $p[3])
       )
       );
             unset($params['']);
             $params['q'] = isset($params['q']) ? 1.0*$params['q'] : $params['q'] = 1.0;
             $list->insert($params, $params['q']);
-        }, preg_split('(\s*,\s*)', $query));
-        return array_values(iterator_to_array($list));
+        }, \preg_split('(\s*,\s*)', $query));
+        return \array_values(\iterator_to_array($list));
     }
 
     public static function bestMatch($acceptables, $choices) {
@@ -48,19 +48,19 @@ class Negotiation {
         unset($parsed['q']);     // Hide quality key from output
         $type = $parsed['type']; // Extract type
         unset($parsed['type']);
-        return implode(';', array_merge([$type], array_map(function ($k, $v): string {
+        return \implode(';', \array_merge([$type], \array_map(function ($k, $v): string {
             return "$k=$v";
-        }, array_keys($parsed), $parsed)));
+        }, \array_keys($parsed), $parsed)));
     }
 
     public function best($choices) {
         $_choices  = self::parse(trim($choices));
         foreach ($this->list as $accept) {
             foreach ($_choices as $choice) {
-                if (preg_match('('.strtr(
-            $accept["type"],
-            [ '.' => '\.', '+' => '\+', '*' => '.+' ]
-        ).')', $choice["type"])) {
+                if (\preg_match('(' . \strtr(
+                        $accept["type"],
+                        [ '.' => '\.', '+' => '\+', '*' => '.+' ]
+                ).')', $choice["type"])) {
                     return self::encodeParsedValue($choice);
                 }
             }
